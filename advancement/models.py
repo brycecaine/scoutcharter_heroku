9e1d61d487b5ebe49770c8e70ce56e2df96eb735
Bryce Caine <cainebryce@gmail.com>
2013-06-04 21:51:23 -0600

    Added models, and configured settings

---
+++ advancement/models.py
@@ -0,0 +1,58 @@
+from django.contrib.auth.models import User
+from django.db import models
+
+class Scout(models.Model):
+	user = models.OneToOneField(User, unique=True)
+	birth_date = models.DateField(blank=True, null=True)
+	patrol = models.CharField(max_length=50, blank=True, null=True)
+	rank = models.CharField(max_length=50, blank=True, null=True)
+	phone_number = models.CharField(max_length=20, blank=True, null=True)
+
+class Parent(models.Model):
+	user = models.OneToOneField(User, unique=True)
+	scouts = models.ManyToManyField(Scout, blank=True, null=True)
+
+class Rank(models.Model):
+	weight = models.IntegerField()
+	name = models.CharField(max_length=30)
+	number_required_meritbadges = models.IntegerField()
+	number_optional_meritbadges = models.IntegerField()
+
+class ScoutRank(models.Model):
+	scout = models.ForeignKeyField(Scout)
+	rank = models.ForeignKeyField(Rank)
+	date_earned = models.DateField(blank=True, null=True)
+	date_awarded = models.DateField(blank=True, null=True)
+
+class MeritBadge(models.Model):
+	name = models.CharField(max_length=100)
+	required = models.BooleanField()
+	alternates = models.CharField(max_length=200)
+
+class ScoutMeritBadge(models.Model):
+	scout = models.ForeignKeyField(Scout)
+	merit_badge = models.ForeignKeyField(MeritBadge)
+	goal_date = models.DateField(blank=True, null=True)
+	date_earned = models.DateField(blank=True, null=True)
+	date_awarded = models.DateField(blank=True, null=True)
+
+class MeritBadgeBook(models.Model):
+	merit_badge = models.ForeignKeyField(MeritBadge)
+	owner = models.CharField(max_length=70)
+	quantity = models.IntegerField()
+
+class ScoutMeritBadgeBook(models.Model):
+	scout = models.ForeignKeyField(Scout)
+	merit_badge_book = models.ForeignKeyField(MeritBadgeBook)
+	date_requested = models.DateField(blank=True, null=True)
+	date_borrowed = models.DateField(blank=True, null=True)
+	date_due = models.DateField(blank=True, null=True)
+	date_returned = models.DateField(blank=True, null=True)
+
+class MeritBadgeCounselor(models.Model):
+	user = models.OneToOneField(User, unique=True)
+	merit_badge = models.ForeignKeyField(MeritBadge)
+	date_requested = models.DateField(blank=True, null=True)
+	date_started = models.DateField(blank=True, null=True)
+	registered = models.BooleanField()
+	active = models.BooleanField()
--- scoutcharter/settings.py
+++ scoutcharter/settings.py
@@ -11,11 +11,11 @@ MANAGERS = ADMINS
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.', # Add 'postgresql_psycopg2', 'mysql', 'sqlite3' or 'oracle'.
-        'NAME': '',                      # Or path to database file if using sqlite3.
+        'ENGINE': 'django.db.backends.sqlite3', # Add 'postgresql_psycopg2', 'mysql', 'sqlite3' or 'oracle'.
+        'NAME': 'scoutcharter_db',                      # Or path to database file if using sqlite3.
         # The following settings are not used with sqlite3:
-        'USER': '',
-        'PASSWORD': '',
+        'USER': 'cainebryce',
+        'PASSWORD': 'scoutcharter765',
         'HOST': '',                      # Empty for localhost through domain sockets or '127.0.0.1' for localhost through TCP.
         'PORT': '',                      # Set to empty string for default.
     }
@@ -157,7 +157,7 @@ LOGGING = {
 
 # Parse database configuration from $DATABASE_URL
 import dj_database_url
-DATABASES['default'] =  dj_database_url.config()
+DATABASES['default'] = dj_database_url.config(default='sqlite:////Users/caine/Dropbox/workspace/scoutcharter/scoutcharter.sqlite') # dj_database_url.config()
 
 # Honor the 'X-Forwarded-Proto' header for request.is_secure()
 SECURE_PROXY_SSL_HEADER = ('HTTP_X_FORWARDED_PROTO', 'https')
