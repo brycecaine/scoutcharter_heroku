ba643926f3c9b8935b3febc60e7fbde0787ca6ab
Bryce Caine <cainebryce@gmail.com>
2013-06-14 21:13:17 -0600

    commented out querysets for now

--- advancement/views.py
+++ advancement/views.py
@@ -1,1 +1,12 @@
-# Create your views here.
+from django.shortcuts import render_to_response
+from django.template import RequestContext
+from advancement.models import ScoutRank, ScoutMeritBadge
+
+def home(request):
+	# user = request.user
+	# scout_ranks = ScoutRank.objects.filter(scout=user)
+	# scout_merit_badges = ScoutMeritBadge.objects.filter(scout=user)
+
+	
+	
+	return render_to_response('index.html', locals(), context_instance=RequestContext(request))
