c0b7712e9d6fa7d714faea9a72dd8df205e67d16
cainebryce <cainebryce@gmail.com>
2013-10-02 04:26:41 +0000

    Trying to hook things up to heroku again

--- scoutcharter/settings.py
+++ scoutcharter/settings.py
@@ -11,10 +11,10 @@ DEBUG = True # For now; but change this to False when live
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.postgresql_psycopg2', # Add 'postgresql_psycopg2', 'mysql', 'sqlite3' or 'oracle'.
-        'NAME': 'd2tcu35dovs8tj',                      # Or path to database file if using sqlite3.
+        'NAME': 'd9279msvqqglg3',                      # Or path to database file if using sqlite3.
         # The following settings are not used with sqlite3:
-        'USER': 'hipfjtdqdpgnis',
-        'PASSWORD': '3sLLuNcp4LY9nW_zg_7AuCikZI',
+        'USER': 'jfnebpdiqntvli',
+        'PASSWORD': 'kcoLlLvLJTo0Ju3B3twgDyDhAs',
         'HOST': 'ec2-54-225-123-71.compute-1.amazonaws.com',                      # Empty for localhost through domain sockets or '127.0.0.1' for localhost through TCP.
         'PORT': '5432',                      # Set to empty string for default.
     }
