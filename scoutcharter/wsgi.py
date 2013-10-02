1e8da61596e8a5d70f7dfc170550515d101fd611
cainebryce <cainebryce@gmail.com>
2013-10-02 04:21:42 +0000

    Trying to hook things up to heroku again

--- scoutcharter/wsgi.py
+++ scoutcharter/wsgi.py
@@ -35,7 +35,7 @@ application = get_wsgi_application()
 sys.path.append('/scoutcharter')
 sys.path.append('/scoutcharter/settings')
 
-from django.core.wsgi import get_wsgi_application
-from dj_static import Cling
+# from django.core.wsgi import get_wsgi_application
+# from dj_static import Cling
 
-application = Cling(get_wsgi_application())
+# application = Cling(get_wsgi_application())
