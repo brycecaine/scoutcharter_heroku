3a5e6be0e610c7904e55c77f0aa3cd308123fafd
cainebryce <cainebryce@gmail.com>
2013-10-02 04:00:56 +0000

    Changed db connection settings for new database

--- scoutcharter/settings.py
+++ scoutcharter/settings.py
@@ -11,11 +11,11 @@ DEBUG = True # For now; but change this to False when live
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.postgresql_psycopg2', # Add 'postgresql_psycopg2', 'mysql', 'sqlite3' or 'oracle'.
-        'NAME': 'deqlp570vcobof',                      # Or path to database file if using sqlite3.
+        'NAME': 'd2tcu35dovs8tj',                      # Or path to database file if using sqlite3.
         # The following settings are not used with sqlite3:
-        'USER': 'szkgmhjuigtjic',
-        'PASSWORD': 'W3CPrXJ0ked_GEjhbJpHv0SMeM',
-        'HOST': 'ec2-54-227-252-82.compute-1.amazonaws.com',                      # Empty for localhost through domain sockets or '127.0.0.1' for localhost through TCP.
+        'USER': 'hipfjtdqdpgnis',
+        'PASSWORD': '3sLLuNcp4LY9nW_zg_7AuCikZI',
+        'HOST': 'ec2-54-225-123-71.compute-1.amazonaws.com',                      # Empty for localhost through domain sockets or '127.0.0.1' for localhost through TCP.
         'PORT': '5432',                      # Set to empty string for default.
     }
 }
