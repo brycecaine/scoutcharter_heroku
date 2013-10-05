78536b53d125f8c340b5b7251ab9d078bf5f055b
cainebryce <cainebryce@gmail.com>
2013-10-05 07:37:03 +0000

    Removing database connection settings in favor of heroku's method

--- scoutcharter/settings.py
+++ scoutcharter/settings.py
@@ -8,18 +8,6 @@ except ImportError, e:
 
 DEBUG = True # For now; but change this to False when live
 
-DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.postgresql_psycopg2', # Add 'postgresql_psycopg2', 'mysql', 'sqlite3' or 'oracle'.
-        'NAME': 'd9279msvqqglg3',                      # Or path to database file if using sqlite3.
-        # The following settings are not used with sqlite3:
-        'USER': 'jfnebpdiqntvli',
-        'PASSWORD': 'kcoLlLvLJTo0Ju3B3twgDyDhAs',
-        'HOST': 'ec2-54-225-123-71.compute-1.amazonaws.com',                      # Empty for localhost through domain sockets or '127.0.0.1' for localhost through TCP.
-        'PORT': '5432',                      # Set to empty string for default.
-    }
-}
-
 PROJECT_ROOT = os.path.abspath(os.path.dirname(__file__))
 PROJECT_DIR = os.path.join(PROJECT_ROOT,'./')
 STATIC_ROOT = os.path.join(PROJECT_DIR, 'static/')
